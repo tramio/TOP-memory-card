6310cff3828fcdba71ac628a8e5988ad1c3b15f2
Tramio <tramiodev@outlook.fr>
2022-04-24 21:28:35 +0200

    implement props from Deck to Card

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,11 +1,11 @@
 import React, { useState } from "react";
 
-const Card = () => {
-  const [title, setTitle] = useState("Odin Odinson");
-  const [description, setDescription] = useState("A very realistic character description");
-  const [imgSrc, setImgSrc] = useState(require('../assets/odin.png'));
+const Card = (props) => {
+  const [title, setTitle] = useState(props.title);
+  const [description, setDescription] = useState(props.description);
+  const [imgSrc, setImgSrc] = useState(props.src);
   return (
-    <div className="card">
+    <div id={props.id} className="card">
       <img src={imgSrc}></img>
       <h1>{title}</h1>
       <p>{description}</p>
--- src/components/Deck.js
+++ src/components/Deck.js
@@ -4,9 +4,16 @@ import Card from "./Card";
 const Deck = () => {
   return (
     <div className="deck">
-      <Card />
-      <Card />
-      <Card />
+      <Card id="card-1" title="Bamboo" description="A very realistic description" src={require("../assets/bamboo.png")} />
+      <Card id="card-2" title="Bear" description="Another description" src={require("../assets/bear.png")} />
+      <Card id="card-3" title="Brown" description="Hi" src={require("../assets/description.png")} />
+      <Card id="card-4" title="Gold" description="Hi" src={require("../assets/gold.png")} />
+      <Card id="card-5" title="Header" description="Hi" src={require("../assets/header.png")} />
+      <Card id="card-6" title="Leaf" description="Hi" src={require("../assets/leaf.png")} />
+      <Card id="card-7" title="Moss" description="Hi" src={require("../assets/moss.png")} />
+      <Card id="card-8" title="Stone" description="Hi" src={require("../assets/stone.png")} />
+      <Card id="card-9" title="Title" description="Hi" src={require("../assets/title.png")} />
+      <Card id="card-10" title="Water" description="Hi" src={require("../assets/water.png")} />
     </div>
   )
 }
