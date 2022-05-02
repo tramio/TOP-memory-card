d6e97b647d2c19b7da1a139e97115ce56712cc75
Tramio <tramiodev@outlook.fr>
2022-05-02 22:24:40 +0200

    lift scores into Main

--- src/components/Deck.js
+++ src/components/Deck.js
@@ -1,48 +1,52 @@
-import { setSelectionRange } from "@testing-library/user-event/dist/utils";
 import React, { useState, useEffect } from "react";
 import Card from "./Card";
 import useCards from "./useCards";
 
-const Deck = () => {
+const Deck = (props) => {
+  const { liftCurrentScore, liftHighScore } = props;
   const [currentCard, setCurrentCard] = useState(null);
   const [clickCounter, setClickCounter] = useState(0);
   function onCardClick(e) {
     setCurrentCard(e.currentTarget.dataset.value);
-    incrementByOne(clickCounter, setClickCounter);
-  }
-  function incrementByOne(state, setState) {
-    let newState = state;
-    newState++;
-    setState(newState);
+    setClickCounter(clickCounter + 1);
   }
 
   const [currentHand, setCurrentHand] = useState([]);
-  useEffect(() => {
+  useEffect(updateHand, [clickCounter]);
+  function updateHand() {
     if (currentHand.includes(currentCard) || currentCard === null) {
       setCurrentHand([]);
     } else {
       let newHand = currentHand.slice().concat(currentCard);
       setCurrentHand(newHand);
     }
-  }, [clickCounter])
+  }
 
   const [currentScore, setCurrentScore] = useState(0);
   useEffect(() => {
+    updateScore();
+    liftCurrentScore(currentScore);
+  }, [clickCounter]);
+  function updateScore() {
     if (currentHand.includes(currentCard)) {
       setCurrentScore(0);
+      console.log("if");
     } else {
-      incrementByOne(currentScore, setCurrentScore);
+      setCurrentScore(currentScore + 1);
+      console.log("else");
     }
-  }, [clickCounter]);
+  };
 
   const [highScore, setHighScore] = useState(0);
-  useEffect(() => {
+  useEffect(updateHighScore, [currentScore]);
+  function updateHighScore() {
     if (currentScore > highScore) {
       setHighScore(currentScore);
+      liftHighScore(highScore);
     }
-  }, [currentScore]);
+  }
 
-  const [baseDeck, setBaseDeck] = useState(useCards());
+  const [baseDeck] = useState(useCards());
   const [randomizedDeck, setRandomizedDeck] = useState(randomize(baseDeck));
   useEffect(() => {
     setRandomizedDeck(randomize(baseDeck));
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,13 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import Deck from "./Deck";
 
 const Main = () => {
+  const [currentScore, setCurrentScore] = useState(0);
+  const [highScore, setHighScore] = useState(0);
+  const liftCurrentScore = (score) => {
+    setCurrentScore(score);
+  }
+  const liftHighScore = (score) => {
+    setHighScore(score);
+  }  
   return (
     <main>
       <h1>Memory Card!</h1>
-      <p>Current score:</p>
-      <p>High score:</p>
-      <Deck />
+      <p>Current score: {currentScore}</p>
+      <p>High score: {highScore}</p>
+      <Deck liftCurrentScore={liftCurrentScore} liftHighScore={liftHighScore}/>
     </main>
   );
 }
